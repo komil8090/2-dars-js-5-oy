9691db573f4302d8c130a86849ee11d91f64bd50
Mirkomil Mirvohidov <mirkomilmirvohidov4@gmail.com>
2024-06-22 14:51:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let ism = prompt("ismigizni kriting") 
+let id= prompt("spaceid yigizni kiritin") 
+let password= prompt("spacePasswordgzni kriting")
+let mirkomilobj = {
+    name: ism,
+    id: id,
+    password: password
+};
+
+console.log(mirkomilobj);
